4699b8dfd5efcb08bac0a1fc53457211d8167639
hamdaouiwassim <hamdaouiwassim@gmail.com>
2020-05-13 16:18:17 +0200

    First Commit

---
+++ src/Controller/VideosController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VideosController extends AbstractController
+{
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function index()
+    {
+        return $this->render('videos/index.html.twig', [
+            'controller_name' => 'VideosController',
+        ]);
+    }
+}
