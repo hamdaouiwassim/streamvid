85f900417930b3c5ae83a8bcedf4f56642ada680
hamdaouiwassim <hamdaouiwassim@gmail.com>
2020-06-15 19:38:38 +0200

    Video Add

--- src/Controller/VideosController.php
+++ src/Controller/VideosController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Video;
 class VideosController extends AbstractController
 {
     /**
@@ -12,8 +14,69 @@ class VideosController extends AbstractController
      */
     public function index()
     {
+        $videos = $this->getDoctrine()
+        ->getRepository(Video::class)
+        ->findAll();
         return $this->render('videos/index.html.twig', [
-            'controller_name' => 'VideosController',
+            'videos' => $videos,
         ]);
     }
+
+
+    /**
+     * @Route("/video/add", name="addvideo")
+     */
+    public function addvideo()
+    {
+        return $this->render('videos/ajoutVideo.html.twig');
+    }
+
+     /**
+     * @Route("/video/add/db", name="addvideodb")
+     */
+    public function addvideodb(Request $request){
+     
+                    //get pic from http request
+                    $Image = $request->files->get('cover'); 
+                    $Content = $request->files->get('content'); 
+                    //dd($Image);
+                    
+                    //generate a unique name for pic
+                    $picName = uniqid().'-'.$Image->getClientOriginalName();
+                      //generate a unique name for pic
+                    $vidName = uniqid().'-'.$Content->getClientOriginalName();
+
+                    //move pic to a directory
+                    $Image->move(
+                            $this->getParameter('covers_directory'),
+                            $picName
+                        );
+                     //move pic to a directory
+                     $Content->move(
+                        $this->getParameter('videos_directory'),
+                        $vidName
+                    );
+
+                    $video = new Video();
+                    $video->setTitle($request->request->get('title'));
+                    $video->setDescription($request->request->get('description'));
+                    $video->setCover($picName);
+                    $video->setContent($vidName);
+                    $video->setAddedAt(new \DateTime(date("Y-m-d H:i:s")));
+                    $video->setModifiedAt(new \DateTime(date("Y-m-d H:i:s")));
+
+                    $entityManager = $this->getDoctrine()->getManager();
+
+                      // tell Doctrine you want to (eventually) save the Product (no queries yet)
+                    $entityManager->persist($video);
+
+                    // actually executes the queries (i.e. the INSERT query)
+                    $entityManager->flush();
+           
+           return new Response("files uploaded");
+            
+
+    }
+
+
 }
--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Entity;
-
+use Symfony\Component\Validator\Constraints as Assert;
 use App\Repository\VideoRepository;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -17,8 +17,121 @@ class Video
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+     /**
+     * @ORM\Column(type="string", length=255)
+     * * @Assert\File(
+     *     mimeTypes = {"image/png",
+     *          "image/jpeg",
+     *           "image/jpg",
+     *          "image/gif"},
+     *     mimeTypesMessage = "Please upload a valid Image File"
+     * )
+     */
+    private $cover;
+
+    /**
+     *  @ORM\Column(type="string", length=255)
+     * * @Assert\File(
+     *     mimeTypes = {"video/mp4", "video/quicktime", "video/avi"},
+     *     mimeTypesMessage = "Please upload a valid Video File"
+     * )
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $added_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modified_at;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(string $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getAddedAt(): ?\DateTimeInterface
+    {
+        return $this->added_at;
+    }
+
+    public function setAddedAt(\DateTimeInterface $added_at): self
+    {
+        $this->added_at = $added_at;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeInterface
+    {
+        return $this->modified_at;
+    }
+
+    public function setModifiedAt(\DateTimeInterface $modified_at): self
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
     }
 }
